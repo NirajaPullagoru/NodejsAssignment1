6c97750eaac2902b29c41d6c97e6739fb577632c
Niraja <p.niraja1@gmail.com>
2018-09-21 10:09:27 +0530

    Modularized code

--- app.js
+++ app.js
@@ -1,71 +1,6 @@
-var http = require('http');
-var fs = require('fs')
-
-// var readStreamm = fs.createReadStream(__dirname + '/config.json', 'utf8');
-
-// readStreamm.on('data', function (chunk) {
-//     var name = JSON.parse(chunk).name;
-//     var phoneNumber = JSON.parse(chunk).phoneNumber;
-//     var emailID = JSON.parse(chunk).emailID;
-// })
-
-
-//console.log("Enter name, phone number and emailID in config.json")
-
-
-// fs.watch('config.json', (eventType, fileName) => {
-//     if (fileName) {
-//         fs.readFile(fileName, 'utf8', function (err, data) {
-
-//             var name = JSON.parse(data).name;
-//             var cell = JSON.parse(data).phoneNumber;
-//             var email = JSON.parse(data).email;
-
-
-//             if (name.length >= 5 && name.length <= 40 && (!(/[^a-zA-Z0-9]/.test(name))))
-//                 console.log("Valid name")
-//             else
-//                 console.log("Invalid name")
-
-//             if (cell.length == 10 && (!(/[^0-9]/.test(cell))))
-//                 console.log("Valid phone number")
-//             else
-//                 console.log("Invalid phone number")
-
-//             var code = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//             if (code.test(String(email).toLowerCase()))
-//                 console.log("Valid email ID")
-//             else
-//                 console.log("Invalid email ID")
-
-//         });
-//     }
-
-// })
-
-
-// const readline = require('readline');
-// const r1 = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
-// // const r2 = readline.createInterface({
-// //     input: process.stdin,
-// //     output: process.stdout
-// // });
-// // const r3 = readline.createInterface({
-// //     input: process.stdin,
-// //     output: process.stdout
-// // });
-
-// r1.question('Enter name: ', (result) => {
-//     if (result.length >= 5 && result.length <= 40 && (!(/[^a-zA-Z0-9]/.test(result)))) {
-//         console.log("Valid name")
-//     } else
-//         console.log("Name should contain only Alphanumeric values. Please re enter the name")
-// })
-
 const readline = require('readline');
+var quests = ['Enter name: ', 'Enter phone number: ', 'Enter email ID: ']
+var i = 0;
 var code = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const rl = readline.createInterface({
     input: process.stdin,
@@ -73,32 +8,59 @@ const rl = readline.createInterface({
     prompt: 'Enter name: '
 });
 
-rl.setPrompt('Enter name: ')
+rl.setPrompt(quests[i])
 rl.prompt();
 
 rl.on('line', (result) => {
-    if (result.length >= 5 && result.length <= 40 && (!(/[^a-zA-Z]/.test(result)))) {
-        console.log("Valid name")
-        rl.setPrompt('Enter phone number: ')
-        rl.prompt();
-
-    } else if (result.length == 10 && (!(/[^0-9]/.test(result)))) {
-        console.log("Valid phone number")
-        rl.setPrompt('Enter email ID: ')
-        rl.prompt();
-    } else if (code.test(String(result).toLowerCase())) {
-        console.log("Valid email ID")
-        console.log('Have a great day!');
-        process.exit(0);
-    } else {
-        //console.log("Invalid name. Please enter only Alphanumeric characters ranging between 5-40 in length")
-        //console.log("Invalid phone number. Please enter a 10 digits number")
-        //console.log("Invalid email ID")
-        console.log("****Invalid input****")
-        rl.setPrompt('Re-Enter the value: ')
-        rl.prompt();
-    }
+    if (i === 0)
+        validName(result)
+    else if (i === 1)
+        validCell(result)
+    else if (i === 2)
+        validEmail(result)
 }).on('close', () => {
     console.log('Have a great day!');
     process.exit(0);
 });
+
+
+var validName = function (result) {
+    if (result.length >= 5 && result.length <= 40 && (!(/[^a-zA-Z0-9]/.test(result)))) {
+        console.log("Valid name")
+        i++;
+        rl.setPrompt(quests[i])
+        rl.prompt();
+    } else {
+        console.log("Invalid name. Please enter only Alphanumeric characters ranging between 5-40 in length")
+        rl.setPrompt('Re-Enter the name: ')
+        rl.prompt();
+    }
+}
+
+var validCell = function (result) {
+    if (result.length == 10 && (!(/[^0-9]/.test(result)))) {
+        console.log("Valid phone number")
+        i++;
+        rl.setPrompt(quests[i])
+        rl.prompt();
+    } else {
+        console.log("Invalid phone number. Please enter a 10 digits number")
+        rl.setPrompt('Re-Enter the phone number: ')
+        rl.prompt();
+    }
+}
+
+var validEmail = function (result) {
+    if (code.test(String(result).toLowerCase())) {
+        console.log("Valid email ID")
+        i++;
+        rl.setPrompt(quests[i])
+        rl.prompt();
+        rl.setPrompt(quests[i])
+        rl.prompt();
+    } else {
+        console.log("Invalid email ID")
+        rl.setPrompt('Re-Enter the email ID: ')
+        rl.prompt();
+    }
+}
